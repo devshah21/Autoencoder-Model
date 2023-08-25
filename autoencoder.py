4e9c29268339bb3a5d30bc5b42a83b9cde5c8804
devshah21 <mail2devshah@gmail.com>
2023-08-24 22:31:23 -0400

    defined build encoder method

---
+++ autoencoder.py
@@ -0,0 +1,38 @@
+from tensorflow.keras import Model
+class Autoencoder:
+    
+    
+    def __init__(self,
+                 input_shape,
+                 conv_filters,
+                 conv_kernels,
+                 conv_strides,
+                 latent_space_dim):
+        self.input_shape = input_shape # [28, 28, 1]
+        self.conv_filters = conv_filters # [2, 4, 8]
+        self.conv_kernels = conv_kernels # [3, 5, 3]
+        self.conv_strides = conv_strides # [1, 2, 2]
+        self.lsd = latent_space_dim # 2
+        
+        self.encoder = None
+        self.decoder = None
+        self.model = None
+        
+        self.num_convlayers = len(conv_filters)
+        
+        self._build()
+        
+        
+    def _build(self):
+        self._build_encoder()
+        self._build_decoder()
+        self._build_autoencoder()
+        
+    def _build_encoder(self):
+        encoder_input = self._add_encoder_input()
+        conv_layers = self._add_conv_layers(encoder_input)
+        bottleneck = self._add_bottleneck(conv_layers) # bottleneck is the output of the encoder
+        self.encoder = Model(encoder_input, bottleneck, name = 'encoder')
+        
+        
+    
