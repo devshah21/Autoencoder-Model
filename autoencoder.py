67011a047b7ac8899526f4fb85d98dae4e2fe159
devshah21 <mail2devshah@gmail.com>
2024-05-25 22:04:17 -0400

    updated autoencoder model

--- autoencoder.py
+++ autoencoder.py
@@ -1,172 +1,117 @@
 from keras import Model
-from keras.layers import Input, Conv2D, ReLU, BatchNormalization, Flatten, Dense, Reshape, Conv2DTranspose, Activation
-from keras import backend as K
+from keras.layers import Input, Conv2D, LeakyReLU, BatchNormalization, Flatten, Dense, Reshape, Conv2DTranspose, Activation
 from keras.optimizers import Adam
 from keras.losses import MeanSquaredError
 import numpy as np
 
 class Autoencoder:
-    
-    
-    def __init__(self,
-                 input_shape,
-                 conv_filters,
-                 conv_kernels,
-                 conv_strides,
-                 latent_space_dim):
-        self.input_shape = input_shape # [28, 28, 1]
-        self.conv_filters = conv_filters # [2, 4, 8]
-        self.conv_kernels = conv_kernels # [3, 5, 3]
-        self.conv_strides = conv_strides # [1, 2, 2]
-        self.lsd = latent_space_dim # 2
-        
+    def __init__(self, input_shape, conv_filters, conv_kernels, conv_strides, latent_space_dim):
+        self.input_shape = input_shape  # [28, 28, 1]
+        self.conv_filters = conv_filters  # [32, 64, 64, 64]
+        self.conv_kernels = conv_kernels  # [3, 3, 3, 3]
+        self.conv_strides = conv_strides  # [1, 2, 2, 1]
+        self.latent_space_dim = latent_space_dim  # 2
+
         self.encoder = None
         self.decoder = None
         self.model = None
-        
-        self.num_convlayers = len(conv_filters)
-        self._shape_before_bn = None
-        self._model_input = None
-        
+
+        self.num_conv_layers = len(conv_filters)
+        self.shape_before_bottleneck = None
+        self.model_input = None
+
         self._build()
-        
+
     def summary(self):
         self.encoder.summary()
         self.decoder.summary()
         self.model.summary()
-        
-    
-    
+
     def _build(self):
         self._build_encoder()
         self._build_decoder()
         self._build_autoencoder()
-    
+
     def _build_autoencoder(self):
-        model_input = self._model_input
+        model_input = self.model_input
         model_output = self.decoder(self.encoder(model_input))
-        self.model = Model(model_input, model_output)
-        
+        self.model = Model(model_input, model_output, name="autoencoder")
+
     def _build_decoder(self):
-        decoder_input = self._add_decoder_input()
-        dense_layer = self._add_dense_layer(decoder_input)
-        reshape_layer = self._add_reshape_layer(dense_layer)
-        conv_transpose_layer = self._add_conv_transpose_layers(reshape_layer)
-        decoder_output = self._add_decoder_output(conv_transpose_layer)
-        self.decoder = Model(decoder_input, decoder_input, name = 'decoder')
-    
-    def _add_decoder_input(self):
-        return Input(shape=self.lsd, name='decoder_input')
-    
-    def _add_dense_layer(self, decoder_input):
-        num_neurons = np.prod(self._shape_before_bn) # [1, 2, 4] -> 8, the np.prod multiplies all the values
-        denselayer = Dense(num_neurons, name = 'decoder_dense')(decoder_input) # this applies the dense layer to the decoder_input
-        return denselayer
-    
-    def _add_reshape_layer(self, dense_layer):
-        return Reshape(self._shape_before_bn)(dense_layer)
-    
-    def _add_conv_transpose_layers(self, x):
-        # loop through conv layers in reverse order, then stop at 1st layer
-        # we want to mirror the convolutional architecture in encoder
-        for i in reversed(range(1, self.num_convlayers)):
-            # [0,1,2] -> [2,1,0], but we want to ignore index 0, hence why we start at 
-            x = self._add_conv_transpose_layer(i, x) # create 1 layer, not multiple layers
-        return x
-    
-    def _add_conv_transpose_layer(self, layer_index, x):
-        layer_num = self.num_convlayers - layer_index
-        conv_transpose_layer = Conv2DTranspose(
-            filters=self.conv_filters[layer_index],
-            kernel_size=self.conv_kernels[layer_index],
-            strides=self.conv_strides[layer_index],
-            padding='same',
-            name=f'decoder_conv_transpose_{layer_num}'
-        )
-        x = conv_transpose_layer(x)
-        x = ReLU(name=f'decoder_relu_{layer_num}')(x)
-        x = BatchNormalization(name = f'decoder_bn_{layer_num}')(x)
-        return x
-    
-    def _add_decoder_output(self, x):
-        conv_transpose_layer = Conv2DTranspose(
-            filters=1, # [24, 24, 1]
+        decoder_input = Input(shape=(self.latent_space_dim,), name="decoder_input")
+        dense_layer = Dense(np.prod(self.shape_before_bottleneck), name="decoder_dense")(decoder_input)
+        reshape_layer = Reshape(self.shape_before_bottleneck, name="decoder_reshape")(dense_layer)
+        conv_transpose_layers = self._add_conv_transpose_layers(reshape_layer)
+        decoder_output = Conv2DTranspose(
+            filters=1,
             kernel_size=self.conv_kernels[0],
             strides=self.conv_strides[0],
             padding='same',
-            name=f'decoder_conv_transpose_{self.num_convlayers}'
-        )
-        x = conv_transpose_layer(x)
-        output_layer = Activation("sigmoid", name='sigmoid')(x)
-        return output_layer
-        
-    
+            name="decoder_output"
+        )(conv_transpose_layers)
+        output_layer = Activation("sigmoid", name="sigmoid")(decoder_output)
+        self.decoder = Model(decoder_input, output_layer, name="decoder")
+
+    def _add_conv_transpose_layers(self, x):
+        for i in reversed(range(1, self.num_conv_layers)):
+            x = Conv2DTranspose(
+                filters=self.conv_filters[i],
+                kernel_size=self.conv_kernels[i],
+                strides=self.conv_strides[i],
+                padding='same',
+                name=f"decoder_conv_transpose_{self.num_conv_layers - i}"
+            )(x)
+            x = LeakyReLU(name=f"decoder_leakyrelu_{self.num_conv_layers - i}")(x)
+            x = BatchNormalization(name=f"decoder_bn_{self.num_conv_layers - i}")(x)
+        return x
+
     def _build_encoder(self):
-        encoder_input = self._add_encoder_input()
+        encoder_input = Input(shape=self.input_shape, name="encoder_input")
         conv_layers = self._add_conv_layers(encoder_input)
-        bottleneck = self._add_bottleneck(conv_layers) # bottleneck is the output of the encoder
-        self._model_input = encoder_input
-        self.encoder = Model(encoder_input, bottleneck, name = 'encoder')
-        
-    def _add_encoder_input(self):
-        return Input(shape=self.input_shape, name = 'encoder_input')
-    
+        bottleneck = self._add_bottleneck(conv_layers)
+        self.model_input = encoder_input
+        self.encoder = Model(encoder_input, bottleneck, name="encoder")
+
     def _add_conv_layers(self, encoder_input):
-        """This method iterates over the number of convolutional layers specified (self.num_convlayers) and adds each convolutional layer to the encoder architecture."""
         x = encoder_input
-        for i in range(self.num_convlayers):
-            x = self._add_conv_layer(i, x)
-        return x   
-           
-    def _add_conv_layer(self, layer_index, x):
-        """ adds a conv block to a graph of layers consisting of
-        conv 2d + ReLU + batch normalization"""
-        layernum = layer_index + 1
-        
-        conv_layer = Conv2D(
-            filters=self.conv_filters[layer_index],
-            kernel_size = self.conv_kernels[layer_index],
-            strides=self.conv_strides[layer_index],
-            padding='same',
-            name=f'encoder_conv_layer_{layernum}'
-        )
-        x = conv_layer(x) # applies defined conv layer to input tensor 'x' -> outputs the output of a conv operation
-        x = ReLU(name=f'encoder_relu_{layernum}')(x)
-        x = BatchNormalization(name=f'encoder_bn_{layernum}')(x)
+        for i in range(self.num_conv_layers):
+            x = Conv2D(
+                filters=self.conv_filters[i],
+                kernel_size=self.conv_kernels[i],
+                strides=self.conv_strides[i],
+                padding='same',
+                name=f"encoder_conv_layer_{i + 1}"
+            )(x)
+            x = LeakyReLU(name=f"encoder_leakyrelu_{i + 1}")(x)
+            x = BatchNormalization(name=f"encoder_bn_{i + 1}")(x)
         return x
 
     def _add_bottleneck(self, x):
-        "flatten data and add bottleneck (Dense Layer)"
-        self._shape_before_bn = K.int_shape(x)[1:] # 4 dimensional array [batchsize, width, height, num of channels]
-        x = Flatten()(x)
-        x = Dense(self.lsd, name = 'encoder_output')(x) # dense layer needs to have number of neurons and that's equal to the latent space dimensions
+        self.shape_before_bottleneck = K.int_shape(x)[1:]
+        x = Flatten(name="encoder_flatten")(x)
+        x = Dense(self.latent_space_dim, name="encoder_output")(x)
         return x
-    
- ### METHODS FOR TESTING ON DATA SET
- 
- 
-    def compile(self, learning_rate=0.0001):   
+
+    def compile(self, learning_rate=0.0001):
         optimizer = Adam(learning_rate=learning_rate)
         mse_loss = MeanSquaredError()
         self.model.compile(optimizer=optimizer, loss=mse_loss)
-        
+
     def train(self, x_train, batch_size, epochs):
-        self.model.fit(x_train, # we pass x_train twice because the expected output is a copy of the input lol
-                       x_train,
-                       batch_size=batch_size,
-                       epochs=epochs,
-                       shuffle=True) 
-    
-    
-    
-        
-        
+        self.model.fit(
+            x_train,
+            x_train,
+            batch_size=batch_size,
+            epochs=epochs,
+            shuffle=True
+        )
+
 if __name__ == '__main__':
     autoencoder = Autoencoder(
-        input_shape=(28,28,1),
-        conv_filters=(32, 64, 64, 64),
-        conv_kernels = (3,3,3,3),
-        conv_strides=(1,2,2,1),
+        input_shape=(28, 28, 1),
+        conv_filters=[32, 64, 64, 64],
+        conv_kernels=[3, 3, 3, 3],
+        conv_strides=[1, 2, 2, 1],
         latent_space_dim=2
     )
     autoencoder.summary()
